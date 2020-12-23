3476b7126361f4ff25999a45350096ddd0ee12f4
dpak1999 <deepakkumardash313@gmail.com>
2020-12-23 20:51:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+import "./App.css";
+
+function App() {
+  return <div className="App">app</div>;
+=======
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App">App</div>;
+>>>>>>> 175d4d8639fc98af0ba18752495c1b8aae48596b
+}
+
+export default App;
