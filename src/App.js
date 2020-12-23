9dd35a4bb6f88e179ff025b3c03d340062ed5654
dpak1999 <deepakkumardash313@gmail.com>
2020-12-24 00:12:16 +0530

    add : home component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavbarComponent from "./Components/NavbarComponent";
 import PopularRepositories from "./Components/PopularRepositories";
+import HomeComponent from "./Components/HomeComponent";
 
 function App() {
   return (
     <Router>
       <NavbarComponent />
+      <Route path="/" exact component={HomeComponent} />
       <Route path="/explore" exact component={PopularRepositories} />
     </Router>
   );
