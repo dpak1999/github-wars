6b0958af4ea6427e65e336daf0f2c09d3e68f571
dpak1999 <deepakkumardash313@gmail.com>
2020-12-24 18:47:57 +0530

    update : styles fro cards

--- src/Components/PopularRepositories.js
+++ src/Components/PopularRepositories.js
@@ -50,12 +50,27 @@ const PopularRepositories = () => {
           <Row>
             {repositories.map((item) => (
               <Col lg={4} sm={10} key={item.id}>
-                <Card className="my-3" style={{ width: "18rem" }}>
-                  <Card.Img variant="top" src={item.owner.avatar_url} />
-                  <Card.Body>
-                    <h1>{item.name}</h1>
+                <Card
+                  className="my-3"
+                  style={{ background: "rgba(11, 83, 69, 0.8)" }}
+                >
+                  <Card.Img
+                    style={{
+                      borderRadius: "50%",
+                      height: "14rem",
+                      width: "14rem",
+                      margin: "auto",
+                      marginTop: "1rem",
+                    }}
+                    fluid
+                    variant="top"
+                    src={item.owner.avatar_url}
+                  />
+                  <Card.Body style={{ textAlign: "center", color: "white" }}>
+                    <Card.Title>{item.name}</Card.Title>
                     <p>
                       <i className="lni lni-user"></i>
+                      <span> </span>
                       {item.full_name}
                     </p>
                     <p>
