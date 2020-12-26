af91ddb422e97c9fe0f1d0f25fb63ba3bbcf53c7
dpak1999 <deepakkumardash313@gmail.com>
2020-12-26 20:47:20 +0530

    fetching user profiles from api

--- src/Components/Battle.js
+++ src/Components/Battle.js
@@ -1,35 +1,58 @@
-import React, { useEffect, useState } from "react";
-import { Form, Row, Col, Container, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Row, Col, Container, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { getUserData } from "../utils/api";
 
 const Battle = () => {
-  getUserData("dpak1999").then((res) => console.log(res));
   const [playerOneSubmit, setPlayerOneSubmit] = useState(false);
   const [playerTwoSubmit, setPlayerTwoSubmit] = useState(false);
   const [playerOne, setPlayerOne] = useState("");
   const [playerTwo, setPlayerTwo] = useState("");
+  const [playerOneData, setPlayerOneData] = useState({});
+  const [playerTwoData, setPlayerTwoData] = useState({});
 
   const handleFirstSubmit = (e) => {
     e.preventDefault();
     setPlayerOneSubmit(true);
+    getUserData(playerOne).then((res) => setPlayerOneData(res));
   };
 
   const handleSecondSubmit = (e) => {
     e.preventDefault();
     setPlayerTwoSubmit(true);
+    getUserData(playerTwo).then((res) => setPlayerTwoData(res));
   };
-
-  useEffect(() => {
-    console.log(playerOne);
-  }, []);
 
   return (
     <Container>
       <h1 className="App">Battle</h1>
       <Row>
         {playerOneSubmit ? (
-          <h1>submitted 1</h1>
+          <Col lg={6} sm={12} key={playerOneData.id}>
+            <Card
+              className="my-3"
+              style={{ background: "rgba(11, 83, 69, 0.8)" }}
+            >
+              <Card.Img
+                style={{
+                  borderRadius: "50%",
+                  height: "14rem",
+                  width: "14rem",
+                  margin: "auto",
+                  marginTop: "1rem",
+                }}
+                variant="top"
+                src={playerOneData.avatar_url}
+              />
+              <Card.Body style={{ textAlign: "center", color: "white" }}>
+                <Card.Title>
+                  <i className="lni lni-user"></i>
+                  <span> </span>
+                  {playerOneData.name}
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
         ) : (
           <Col md={6} sm={12}>
             <Form onSubmit={handleFirstSubmit} className="button-battle">
@@ -54,7 +77,31 @@ const Battle = () => {
         )}
 
         {playerTwoSubmit ? (
-          <h1>submitted 2</h1>
+          <Col lg={6} sm={12} key={playerTwoData.id}>
+            <Card
+              className="my-3"
+              style={{ background: "rgba(11, 83, 69, 0.8)" }}
+            >
+              <Card.Img
+                style={{
+                  borderRadius: "50%",
+                  height: "14rem",
+                  width: "14rem",
+                  margin: "auto",
+                  marginTop: "1rem",
+                }}
+                variant="top"
+                src={playerTwoData.avatar_url}
+              />
+              <Card.Body style={{ textAlign: "center", color: "white" }}>
+                <Card.Title>
+                  <i className="lni lni-user"></i>
+                  <span> </span>
+                  {playerTwoData.name}
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
         ) : (
           <Col md={6} sm={12}>
             <Form onSubmit={handleSecondSubmit} className="button-battle">
@@ -78,13 +125,11 @@ const Battle = () => {
           </Col>
         )}
       </Row>
-      <div>
-        {playerOneSubmit && playerTwoSubmit ? (
+      <div className="button-battle">
+        {playerOneSubmit && playerTwoSubmit && (
           <Link to="/results">
             <Button>Battle</Button>
           </Link>
-        ) : (
-          ""
         )}
       </div>
     </Container>
